d1d2fea2f2f36ff1cd6e7ecad70f8769999a82cc
ubep <u.biallas@epages.com>
2015-08-17 09:17:41 +0200

    refactored main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var sortBy = require('sort-array')
 var restify = require('restify')
-
+var rsvp = require('rsvp')
 var Board = require('./Board')
 var JiraQuerry = require('./JiraQuerry')
 var Sprint = require('./Sprint')
 var SprintDataExtractor = require('./SprintDataExtractor')
-var EPPromise = require('./EPPromise')
+
+
 
 var port = 3001
 
@@ -27,7 +28,7 @@ server.get("/jiradata/sprinthistory", function(req, res, next) {
 
   var board = new Board(boardId)
   board.getSprints().then(function(result) {
-    handleSprints(boardId, result).success(function(sprintHistory) {
+    handleSprints(boardId, result).then(function(sprintHistory) {
       res.json(sprintHistory);
     })
   })
@@ -51,27 +52,29 @@ server.listen(port, function (err) {
 function handleSprints(boardId, result) {
   var order = [];
   var sprintHistory = [];
-  var epp = new EPPromise()
-  for(var i = 0; i < result.length; i++) {
-    order.push(result[i]);
+  var promise = new rsvp.Promise(function(resolve) {
 
-    var sprint = new Sprint(boardId, result[i])
-    sprint.getSprint().then(function(sprint) {
-        sprintDataExtractor = new SprintDataExtractor()
-        sprintHistory.push(sprintDataExtractor.extractData(sprint))
-        if(sprintHistory.length == result.length) {
-          sortBy(sprintHistory, "id", { id: order })
-          jiraQuerry = new JiraQuerry()
-          var currentSprintname = sprintHistory[result.length-1].sprintName
-          jiraQuerry.getSprintGoals(currentSprintname).success( function(querryResult) {
-            var sprintGoals = sprintDataExtractor.extractSprintGoals(querryResult);
-            sprintHistory[result.length-1].sprintGoals = sprintGoals;
-            epp.getCallback().callback(sprintHistory)
-          })
+    for(var i = 0; i < result.length; i++) {
+      order.push(result[i]);
 
-        }
-    });
+      var sprint = new Sprint(boardId, result[i])
+      sprint.getSprint().then(function(sprint) {
+          sprintDataExtractor = new SprintDataExtractor()
+          sprintHistory.push(sprintDataExtractor.extractData(sprint))
+          if(sprintHistory.length == result.length) {
+            sortBy(sprintHistory, "id", { id: order })
+            jiraQuerry = new JiraQuerry()
+            var currentSprintname = sprintHistory[result.length-1].sprintName
+            jiraQuerry.getSprintGoals(currentSprintname).success( function(querryResult) {
+              var sprintGoals = sprintDataExtractor.extractSprintGoals(querryResult);
+              sprintHistory[result.length-1].sprintGoals = sprintGoals;
+              resolve(sprintHistory)
+            })
 
-  }
-  return epp
+          }
+      })
+    }
+
+  })
+  return promise
 }
